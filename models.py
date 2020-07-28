26a19ba3046c7c512cc4382274288bbd7f1608a6
guoclara <48026886+guoclara@users.noreply.github.com>
2020-07-28 15:50:44 -0700

    added a trips class w columns

--- models.py
+++ models.py
@@ -1,13 +1,33 @@
 from app import db
+from sqlalchemy import CheckConstraint
 
 class User(db.Model):
     # Table columns
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, index=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    fullname = column_property(first_name+" "+last_name)
+    weight = dbColumn(db.Float(1, 10), nullable=False, default=1.0)
 
     # String representation of this object (for logging)
     def __repr__(self):
         return '<User %r>' % self.username
 
 # Define an arbitrary amount of these classes below...
+
+class Trips(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, index=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    description = db.Column(db.String(500))
+    signup_deadline = db.Column(db.DateTime(timezone=False), nullable=False)
+    price = db.Column(db.Float(3, 2), nullable=False)
+    car_cap = db.Column(db.Integer)
+    noncar_cap = db.Column(db.Integer, nullable=False)
+
+    # String representation of this object (for logging)
+    def __repr__(self):
+        return '<Trip %r>' % self.name
