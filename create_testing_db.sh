200d7b08dc69921c4a439cc427311559bd440fc0
ethanga12 <66858278+ethanga12@users.noreply.github.com>
2021-01-16 15:26:06 -0600

    johnny and ethan edits

--- create_testing_db.sh
+++ create_testing_db.sh
@@ -3,6 +3,5 @@
 # run w this command: bash create_testing_db.sh
 # must be in the project brown outing club directory when running (for the boc_dump.sql filepath)
 
-echo "enter your mysql password if you have it, else just press enter"
-mysql -uroot -p -e "CREATE DATABASE IF NOT EXISTS boc_test;"
-mysql -uroot -p boc_test < boc_dump.sql
+sudo mysql -uroot -e "CREATE DATABASE IF NOT EXISTS boc_test; GRANT ALL PRIVILEGES ON *.* TO 'tester'@'localhost' IDENTIFIED BY 'password';"
+sudo mysql -uroot boc_test < boc_dump.sql
